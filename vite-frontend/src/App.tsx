e4191e36e43f50461a3e8834592fd3bc8946780d
Zeeshawn Hasnain <zeeshawnh@Mac.northstate.net>
2025-08-08 15:04:29 -0400

    Changed heading and working on layout

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import MapContainer from "./components/Map/MapContainer";
+import "./App.css";
 
 function App() {
   return (
     <div id="root">
-      <h1>Electoral Contributions Map</h1>
-      <p className="app-description">
-        This map shows the top 10 recipients of campaign contributions in each
-        state.
-      </p>
-      <MapContainer />
+      <main>
+        <h1>How Each State Voted With Their Money</h1>
+        <p className="app-description">
+          This map shows the top 10 recipients of campaign contributions in each
+          state.
+        </p>
+        <MapContainer />
+      </main>
+      <div className="sidebar">
+
+      </div>
     </div>
   );
 }
