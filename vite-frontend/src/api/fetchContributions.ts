dc3aea828001d95dca44dff2a011139816228d35
ZeeshawnH <zeeisawesome99@gmail.com>
2026-01-06 00:29:49 -0500

    Fixed api route

--- vite-frontend/src/api/fetchContributions.ts
+++ vite-frontend/src/api/fetchContributions.ts
@@ -13,12 +13,13 @@ export const fetchContributionsByState = async () => {
   }
 };
 
-export const fetchContributionsWithCandidates = async () => {
-  const url = "localhost:8080";
+export const fetchContributionsWithCandidates = async (year?: number) => {
+  const url = import.meta.env.VITE_API_URL;
   const path = "/api/contributions/withCandidates";
 
   try {
-    const response = await axios.get(`http://${url}${path}`);
+    const query = year ? `?year=${year}` : "";
+    const response = await axios.get(`http://${url}${path}${query}`);
     return response.data;
   } catch (error) {
     console.error(error);
