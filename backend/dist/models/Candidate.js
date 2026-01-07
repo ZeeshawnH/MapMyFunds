97ef6a4c03a6b3d1ac282fb73729abcb76d6d92d
ZeeshawnH <zeeisawesome99@gmail.com>
2026-01-07 13:44:25 -0500

    Removed old services

--- backend/dist/models/Candidate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Candidate = void 0;
-const mongoose_1 = require("mongoose");
-const candidateSchema = new mongoose_1.Schema({
-    candidate_id: { type: String, required: true, unique: true },
-}, { strict: false });
-exports.Candidate = (0, mongoose_1.model)('Candidate', candidateSchema);
